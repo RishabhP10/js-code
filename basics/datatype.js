38b2d96060971e0b7188c178efb6c1a06169b049
RishabhP10 <78598770+RishabhP10@users.noreply.github.com>
2024-08-21 12:22:13 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS cpde as newer version
+//alert(3+3) // we are using nodejs , not browers
+//alert is use in brower console
+
+
+
+let age  = 18
+let isLoggedIn = false
+let state;
+
+
+//number = 2 to power 53
+// bigint
+//string ""
+//boolean true/false
+//null standalone value
+//undefined
+//symbol unique 
+
+
+console.log(typeof "rishabh");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+
+
+ 
---
+++ basics/variable.js
@@ -0,0 +1,23 @@
+const accountId = 1234
+let accountEmail = "rishabh@gmail.com"
+var accountPassword = "1221"
+accountCity = "Nagpur"
+let accountState
+
+
+//accountId = 1111
+accountEmail = "abc@gmail.com"
+accountPassword = "1111"
+accountCity = "Jabalpur"
+
+//console.log(accountId); // const values is not changable
+//console.log(accountEmail);
+
+/*
+do not prefer var 
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
