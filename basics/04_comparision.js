ba131015359cdd32371676dd685dfc48f79a47b2
RishabhP10 <78598770+RishabhP10@users.noreply.github.com>
2024-08-22 14:06:35 +0000

    comparision done

---
+++ basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0); 
+console.log(null == 0);
+console.log(null >= 0); //true
+console.log(null <= 0); //true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0); //false
+
+// === strictly check not only value also check data type
+
+console.log("2"==2) // true value is same not check data type
+console.log("2" === 2); // false also check the data type
+
+
+
