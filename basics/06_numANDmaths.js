461b857825801056a4e55c8e2a6974de678d4901
RishabhP10 <78598770+RishabhP10@users.noreply.github.com>
2024-08-27 07:34:11 +0000

    maths_function

---
+++ basics/06_numANDmaths.js
@@ -0,0 +1,48 @@
+// const score = 8090
+// const balance  = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// // convert into string typeof
+// // now if we check length 
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 67.9876
+// console.log(otherNumber.toPrecision(4));
+
+
+// const one = 1000000
+// // to calculates zero's
+// console.log(one.toLocaleString()); // US Numbers
+// console.log(one.toLocaleString('en-IN')); // INDIAN values
+
+// -----------------------Maths----
+
+// console.log(Math);
+// object 
+// various values , inspect 
+
+// console.log(Math.abs(-199));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.9));
+// console.log(Math.max(4,5,6,1));
+
+
+// random value lie btw 0-1
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max-min +1 ) + min));
