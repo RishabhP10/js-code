8c9e8fed976b7aa3c4703dbe3f713257c2c45a91
RishabhP10 <78598770+RishabhP10@users.noreply.github.com>
2024-09-03 11:58:14 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,10 +20,76 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
 // optional Chaining ..
 // console.log(regularUser.fullname?.userfullname.firstname);
 
 
-const obj1 = {1:"a", 2:"b"}
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+
+// const obj3 = {obj1 , obj2}
+// object inside objects
+// console.log(obj3);
+
+// const obj3 = Object.assign(obj1,obj2)
+// good practice empty value ,target and other is source
+// const obj3 = Object.assign({},obj1,obj2)
+
+// spered operator...
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const user = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
+user[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('name'));  // result in boolean
+
+
+
+const course = {
+
+    coursename: "js",
+    courseprice: 999,
+    courseInstructor: "Rakesh"
+    
+}
+
+const {courseInstructor} = course
+const {courseInstructor: Instructor} = course
+
+console.log(Instructor);
+
+// API..............
+// {
+//     "NAME" : "Rishabh Pathak",
+//     "GENDER" : "Male",
+//     "Price" : "Free"
+// }
+
+// In the form of array
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
 
